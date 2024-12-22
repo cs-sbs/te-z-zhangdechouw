02f5ac40c244123de962c7d69eaa12ce231ae79e
zhangdechouw <2189909219@qq.com>
2024-12-22 22:49:13 +0800

    Update NumberFeature.java

--- src/main/java/org/example/NumberFeature.java
+++ src/main/java/org/example/NumberFeature.java
@@ -10,8 +10,23 @@ public class NumberFeature {
         inputNumber = in.nextInt();
         // 你的代码逻辑
 
-        result = inputNumber;
+        digital_eigenvalue(inputNumber, result);
 
+
+    }
+
+    public static void digital_eigenvalue(int inputNumber, int result) {
+        int number;
+        int digit_position = 1;
+        int binary_bit_value = 1;
+        while (inputNumber > 0) {
+            number = inputNumber % 10;
+            inputNumber = inputNumber / 10;
+            if (number % 2 == digit_position % 2)
+                result += binary_bit_value;
+            binary_bit_value *= 2;
+            digit_position++;
+        }
         System.out.println(result);
     }
 }
